4d80879402e71d06ae526b75acccfec1a970030c
Do Hun <119511836+do-huni@users.noreply.github.com>
2023-03-22 23:44:24 +0900

    Delete Variable directory

--- Variable/Hello.java
+++
@@ -1,6 +0,0 @@
-class Hello{ //만약 public class를 지정한다면 public class의 명칭과 소스파일(xxx.java)의 명칭은 같아야한다.
-    public static void main(String[] args){
-        System.out.println("Hello, world.");
-    }
-    /* 범위 주석. JS와 동일 */
-}
